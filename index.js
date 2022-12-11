a33a50c67fd050560aa254517c14bfa14afdae33
Chantell Davis <chantelldavis2014@yahoo.com>
2022-12-11 18:29:24 -0500

    commit tree f727b74657c37d3c56a7004a3e1af326e3428afa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards){
+  let i = 0; 
+  while (i < cards.length) {
+    console.log(` "Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    i++; 
+  }
+
+  return cards;
+}
+
+writeCards(cards);
